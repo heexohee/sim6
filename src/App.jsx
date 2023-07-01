777463c75c6239eb18894b840c7e50d663b101ec
heexohee <rsoy2918@naver.com>
2023-07-01 19:11:24 +0900

    usecallback마무리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Box1 from "./components/Box1";
 import Box2 from "./components/Box2";
 import Box3 from "./components/Box3";
+
 
 
 const boxesStyle = {
@@ -26,9 +27,19 @@ function App() {
   };
 
   // count를 초기화해주는 함수. 
-const initCount = () =>{
+  // 우리의 목적은 count가 됬을 때, 
+  // box1 컴포넌트가 렌더링 되지 않게하는 것!
+const initCount = useCallback(() =>{
+  console.log(`${count}에서 0으로 변경되었슴다.`);
   setCount(0);
-}
+}, [count]);
+// 유즈콜백도 특정 스테이트가 변경될 때, 처음에 저장했던 콜백함수가
+// 갱신되어야하면 이 의존성 배열에 그 해당 state를 넣어야만 함. 
+// 그래서 의존성 배열 공간을 만들어 줬던 것.
+// 의존성 배열의 값이 비어있으면, usecallback이 count가 0일
+// 때의 시점을 기준으로 메모리에 함수를 저장했기 때문임. 
+// 그래서 기존 코드의 의존성 배열에 count를 넣으면 count가 변경될 때마다 새롭게 함수를 할당함.
+
 
   return (
     <>
