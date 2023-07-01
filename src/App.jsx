3e74e9ba22df19ee01e71592d50fb31cc0359f70
heexohee <rsoy2918@naver.com>
2023-07-01 17:55:41 +0900

    부모가 바뀌었는데 자식컴포까지 랜더링 됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+
+
+const boxesStyle = {
+  display: "flex",
+  marginTop: "10px",
+};
+
 
 
 function App() {
-
-  console.log('App 컴포넌트가 렌더링되었씁니다');
+  console.log("App 컴포넌트가 렌더링되었씁니다");
   const [count, setCount] = useState(0);
 
-
   // 1증가 함수
-  const onPlusButtonClickButtonHandler = () =>
-  {
-    setCount(count+1);
-  }
-
+  const onPlusButtonClickButtonHandler = () => {
+    setCount(count + 1);
+  };
 
   // 1감소 함수
-  const onMinusButtonClickButtonHandler = () =>
-  {
-    setCount(count-1);
-  }
+  const onMinusButtonClickButtonHandler = () => {
+    setCount(count - 1);
+  };
 
-
-  return <>
-  <h3>카운트 예제</h3>
-  <p>현재 카운트 : {count}임</p>
-<button onClick={onPlusButtonClickButtonHandler}>+</button>
-<button onClick={onMinusButtonClickButtonHandler}>-</button>
-
-  </>
-  ;
+  return (
+    <>
+      <h3>카운트 예제</h3>
+      <p>현재 카운트 : {count}임</p>
+      <button onClick={onPlusButtonClickButtonHandler}>+</button>
+      <button onClick={onMinusButtonClickButtonHandler}>-</button>
+      <div style={boxesStyle}>
+        <Box1 />
+        <Box2 />
+        <Box3 />
+      </div>
+    </>
+  );
 }
 
 export default App;
